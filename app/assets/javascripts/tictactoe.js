4cd839cb6d426b3f364f760670ef5a66360b11a4
Arye Eidelman <aryedov.e@gmail.com>
2019-07-11 05:55:16 -0400

    Passing 10 tests

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,18 @@
-// Code your JavaScript / jQuery solution here
+// Must be global scoped for tests
+var turn = 0
+
+function player(){
+  return turn / 2 ? "O" : "X"
+}
+
+function updateState(){
+
+}
+
+function doTurn(){
+  turn++
+}
+
+function attachListeners(){
+
+}
